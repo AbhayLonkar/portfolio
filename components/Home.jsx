979b7aa383a575a4ec9d0966ffb4421f7da166d1
Abhay Lonkar <abhaylonkar9@gmail.com>
2025-05-02 14:49:17 +0530

    Updaged About Section

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,37 +1,39 @@
 import React from 'react'
-import { AiOutlineCloudDownload} from "react-icons/ai";
-import {GrLinkedin, GrTwitter} from "react-icons/gr";
+import { AiOutlineCloudDownload } from "react-icons/ai";
+import { GrLinkedin, GrTwitter } from "react-icons/gr";
 
 const Home = () => {
     return (
-        <section id={'home'} className={' mt-10 md:h-dvh  font-mono flex  justify-center  dark:text-white text-gray-500 pb-16'}>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-2  justify-center h-max">
+        <section id={'home'} className={' mt-10 md:h-full  font-mono flex  justify-center  dark:text-white text-gray-600 pb-16'}>
+            <div className='w-300 m-auto'>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-2  justify-center h-max">
 
-                <div className={'md:p-10 md:px-5 flex gap-2 md:flex-row flex-row-reverse justify-center md:justify-end items-start'}>
-                    <div className={'p-1 flex flex-col justify-center  gap-2 h-full'}>
-                        <GrLinkedin size={30} className={'text-blue-600'} />
-                        <GrTwitter size={30} className={'text-blue-600'} />
+                    <div className={'md:p-10 md:px-5 flex gap-2 md:flex-row flex-row-reverse justify-center md:justify-end items-start'}>
+                        <div className={'p-1 flex flex-col justify-center  gap-2 h-full'}>
+                            <GrLinkedin size={30} className={'text-blue-600'} />
+                            <GrTwitter size={30} className={'text-blue-600'} />
+                        </div>
+                        <img src={'/images/Profile.png'} alt={'Profile'} className={'w-65'} />
                     </div>
-                    <img src={'/images/Profile.png'} alt={'Profile'} className={'w-65'} />
-                </div>
 
-                <div className={'flex justify-center md:justify-start'}>
-                <div className={'md:py-10 md:px-5 py-5 p-1 w-4/5 text-left'}>
-                        <h4 className={'md:text-lg text-md m-1'}>
-                            <span className={'text-red-500'}>&lt;span&gt;</span> Hey, I'm Abhay <span className={'text-red-500'}>&lt;/span&gt;</span>
-                        </h4>
-                        <p className={'text-3xl  my-3'}>
-                            <span className={'text-green-600'}> &#123;Full Stack&#125;</span> Web & App Developer
-                        </p>
-                        <p className={'m-1 text-md '}>
-                            <span className={'text-red-500'}>&lt;p&gt;</span>
-                            With expertise in cutting-edge technologies such as <span className={'text-red-500'}>NodeJS, React, TailwindCSS</span> and <span className={'text-red-500'}>React Native</span>... I deliver web solutions that are both innovative and robust.
-                            <span className={'text-red-500'}>&lt;/p&gt;</span>
-                        </p>
-                        <button className={'mt-5 h-12 w-37 p-2 font-mono hover:bg-blue-600 rounded-full hover:text-white flex items-center justify-evenly gap-1 shadow-md transition-all hover:cursor-pointer border border-blue-600 bg-transparent text-blue-600'}>
-                            <AiOutlineCloudDownload size={20} /> Download CV
-                        </button>
-                </div>
+                    <div className={'flex justify-center md:justify-start'}>
+                        <div className={'md:py-10 md:px-5 py-5 p-1 w-4/5 text-left'}>
+                            <h4 className={'md:text-lg text-md m-1'}>
+                                <span className={'text-red-500'}>&lt;span&gt;</span> Hey, I'm Abhay <span className={'text-red-500'}>&lt;/span&gt;</span>
+                            </h4>
+                            <p className={'text-3xl  my-3'}>
+                                <span className={'text-green-600'}> &#123;Full Stack&#125;</span> Web & App Developer
+                            </p>
+                            <p className={'m-1 text-md '}>
+                                <span className={'text-red-500'}>&lt;p&gt;</span>
+                                With expertise in cutting-edge technologies such as <span className={'text-red-500'}>NodeJS, React, TailwindCSS</span> and <span className={'text-red-500'}>React Native</span>... I deliver web solutions that are both innovative and robust.
+                                <span className={'text-red-500'}>&lt;/p&gt;</span>
+                            </p>
+                            <button className={'mt-5 h-12 w-37 p-2 font-mono hover:bg-gray-600 rounded-full hover:text-white flex items-center justify-evenly gap-1 shadow-md transition-colors hover:cursor-pointer border border-gray-600 bg-transparent text-gray-600 dark:border-white dark:text-white dark:hover:bg-white dark:hover:text-black'}>
+                                <AiOutlineCloudDownload size={20} /> Download CV
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,13 @@
-import React, {useEffect, useState} from 'react'
-import {IoMdHome} from "react-icons/io";
-import {FaUser} from "react-icons/fa";
-import {SiPolymerproject} from "react-icons/si";
-import {TiContacts} from "react-icons/ti";
-import {AiOutlineGithub} from "react-icons/ai";
-import {MdDarkMode, MdLightMode} from "react-icons/md";
+import React, { useEffect, useState } from 'react'
+import { IoMdHome } from "react-icons/io";
+import { FaUser } from "react-icons/fa";
+import { SiPolymerproject } from "react-icons/si";
+import { TiContacts } from "react-icons/ti";
+import { AiOutlineGithub } from "react-icons/ai";
+import { MdDarkMode, MdLightMode } from "react-icons/md";
 
-const Navbar = ({activeTab, setActiveTab}) => {
-    const [darkMode, setDarkMode] = useState(true);
+const Navbar = ({ activeTab, setActiveTab }) => {
+    const [darkMode, setDarkMode] = useState(false);
 
     const toggleDarkMode = () => {
         setDarkMode(prev => !prev);
@@ -21,18 +21,18 @@ const Navbar = ({activeTab, setActiveTab}) => {
         }
     }, [darkMode]);
 
-const tabs = [
-    {name: "Home", icon: <IoMdHome size={22} className={activeTab === "Home" ? 'md:text-black text-red-600' : ''} />},
-    {name: "About", icon: <FaUser size={18}  className={activeTab === "About" ? 'md:text-black text-red-600' : ''}/>},
-    {name: "Project", icon: <SiPolymerproject size={20}  className={activeTab === "Project" ? 'md:text-black text-red-600' : ''}/>},
-    {name: "Contact", icon: <TiContacts size={20}  className={activeTab === "Contact" ? 'md:text-black text-red-600' : ''}/>},
-]
+    const tabs = [
+        { name: "Home", icon: <IoMdHome size={22} className={activeTab === "Home" ? 'md:text-black text-red-600' : ''} /> },
+        { name: "About", icon: <FaUser size={18} className={activeTab === "About" ? 'md:text-black text-red-600' : ''} /> },
+        { name: "Project", icon: <SiPolymerproject size={20} className={activeTab === "Project" ? 'md:text-black text-red-600' : ''} /> },
+        { name: "Contact", icon: <TiContacts size={20} className={activeTab === "Contact" ? 'md:text-black text-red-600' : ''} /> },
+    ]
 
     return (<>
-        <nav className='flex justify-around p-3 h-10 items-center '>
+        <nav className='flex md:justify-around justify-between p-5  h-10 items-center '>
             <h3 className='text-2xl font-bold font-mono dark:text-white'>Abhay Lonkar</h3>
-            <div className={'flex items-center justify-center gap-4'}>
-                <AiOutlineGithub size={30}className={'dark:invert'} />
+            <div className={'flex items-center justify-center gap-4 '}>
+                <AiOutlineGithub size={30} className={'dark:invert'} />
                 {darkMode ? <MdLightMode size={30} className={`cursor-pointer dark:invert`} onClick={toggleDarkMode} /> :
                     <MdDarkMode size={30} className={'cursor-pointer'} onClick={toggleDarkMode} />}
             </div>
@@ -43,17 +43,17 @@ const tabs = [
                     return (
                         <li
                             key={index}
-                            className={`p-2 px-4  rounded-3xl flex gap-2 cursor-pointer transition-colors ease-linear ${activeTab === tab.name? 'bg-slate-300': ''}`}
+                            className={`p-2 px-4  rounded-3xl flex gap-2 cursor-pointer transition-colors ease-linear ${activeTab === tab.name ? 'bg-slate-300' : ''}`}
                             onClick={() => setActiveTab(tab.name)}
                         >
-                            {tab.icon} {activeTab === tab.name ? "" : tab.name }
+                            {tab.icon} {activeTab === tab.name ? "" : tab.name}
                         </li>
                     )
                 })}
             </ul>
         </nav>
 
-        <nav className={'fixed z-10 bottom-0 right-0 border-t-1 w-full flex items-center h-15 rounded-t-2xl bg-white dark:bg-gray-900 md:hidden'}>
+        <nav className={'fixed z-10 bottom-0 right-0 border-t-1 w-full flex items-center h-15 rounded-t-2xl bg-white dark:bg-gray-900 dark:border-t-green-600 md:hidden'}>
             <ul className={'grid grid-cols-4 w-full'}>
                 {tabs.map((tab, index) => {
                     return (
@@ -62,13 +62,13 @@ const tabs = [
                             className={`p-2 px-4 flex flex-col items-center justify-center gap-2 cursor-pointer transition-colors dark:text-white text-xs `}
                             onClick={() => setActiveTab(tab.name)}
                         >
-                            {tab.icon} {tab.name }
+                            {tab.icon} {tab.name}
                         </li>
                     )
                 })}
             </ul>
         </nav>
-        </>
+    </>
     )
 }
 export default Navbar

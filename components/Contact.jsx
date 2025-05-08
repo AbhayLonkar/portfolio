edf2a8b801ed1ba457caa3e3a1d9a34239cd11f2
Abhay Lonkar <abhaylonkar9@gmail.com>
2025-05-08 15:31:44 +0530

    edit contact section

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Contact = () => {
     return (
         <section id='contact' className={'mt-10 md:h-full  font-mono flex flex-col items-centers justify-center  dark:text-white text-gray-600 pb-16'}>
-            <h1 className={'text-center text-4xl text-black mb-10 font-bold dark:text-white'}>Contact Me</h1>
+            <h1 className={'text-center text-4xl text-black mb-1 font-bold dark:text-white'}>
+                Get in <span className='text-blue-400'>Touch</span>
+            </h1>
+            <h3 className={'text-center text-lg text-gray-500 dark:text-gray-200 '}>Have feedback or need some help?</h3>
 
         </section>
     )
